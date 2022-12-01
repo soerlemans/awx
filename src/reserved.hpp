1e45981eb60a949e5d4c79cf95594b46fecd712a
sappigeninja <sebasoerlemans@gmail.com>
2022-12-02 00:05:26 +0100

    reserved.hpp is now correct

--- src/reserved.hpp
+++ src/reserved.hpp
@@ -19,13 +19,13 @@
 // Concepts:
 // The Identifier is either one character or
 template<typename T>
-concept ReservedIdentifier = std::is_convertible_v<T, std::string_view>
-                             || std::same_as<std::remove_cv<T>, char>;
+concept ReservedIdentifier =
+  std::is_convertible_v<T, std::string_view> || std::same_as<T, char>;
 
 // AWX reserved keywords and symbols
 namespace reserved {
 // Helper class for the Reserved global variable definitions
-template<typename T>
+template<typename T = char>
   requires ReservedIdentifier<T>
 class ReservedWrapper {
   private:
@@ -33,7 +33,7 @@ class ReservedWrapper {
   const TokenType m_tokentype;
 
   public:
-  constexpr ReservedWrapper(T t_indentifier, TokenType t_tokentype = TokenType::UNKNOWN)
+  constexpr ReservedWrapper(T t_indentifier, TokenType t_tokentype)
     : m_identifier{t_indentifier}, m_tokentype{t_tokentype}
   {}
 
@@ -58,76 +58,81 @@ class ReservedWrapper {
 // clang-format off
 // Language reserved keywords
 namespace keywords {
-  DEFINE_RESERVED(g_function, "function", TokenType::FUNCTION_KEYWORD);
-  DEFINE_RESERVED(g_if, "if", TokenType::IF_KEYWORD);
-  DEFINE_RESERVED(g_else, "else", TokenType::ELSE_KEYWORD);
-  DEFINE_RESERVED(g_do, "do", TokenType::DO_KEYWORD);
-  DEFINE_RESERVED(g_while, "while", TokenType::WHILE_KEYWORD);
-  DEFINE_RESERVED(g_for, "for", TokenType::FOR_KEYWORD);
-  DEFINE_RESERVED(g_in, "in", TokenType::IN_KEYWORD);
+  DEFINE_RESERVED(g_function, "function", FUNCTION_KEYWORD);
+  DEFINE_RESERVED(g_if, "if", IF_KEYWORD);
+  DEFINE_RESERVED(g_else, "else", ELSE_KEYWORD);
+  DEFINE_RESERVED(g_do, "do", DO_KEYWORD);
+  DEFINE_RESERVED(g_while, "while", WHILE_KEYWORD);
+  DEFINE_RESERVED(g_for, "for", FOR_KEYWORD);
+  DEFINE_RESERVED(g_in, "in", IN_KEYWORD);
 
   constexpr u8 keywords_size{7};
-  std::array<ReservedWrapper, keywords_size> m_keywords{}
+  std::array<ReservedWrapper<const char*>, keywords_size> m_keywords{
+	g_function,
+	g_if, g_else,
+	g_do, g_while,
+	g_for, g_in
+  };
 }; // namespace keywords
 
 // Language reserved symbols
 namespace symbols {
   // Braces
-  DEFINE_RESERVED(g_paren_open,     '(', TokenType::PAREN_OPEN);
-  DEFINE_RESERVED(g_paren_close,    ')', TokenType::PAREN_CLOSE);
-  DEFINE_RESERVED(g_accolade_open,  '{', TokenType::ACCOLADE_OPEN);
-  DEFINE_RESERVED(g_accolade_close, '}', TokenType::ACCOLADE_CLOSE);
-  DEFINE_RESERVED(g_brace_open,     '[', TokenType::BRACE_OPEN);
-  DEFINE_RESERVED(g_brace_close,    ']', TokenType::BRACE_CLOSE);
+  DEFINE_RESERVED(g_paren_open,     '(', PAREN_OPEN);
+  DEFINE_RESERVED(g_paren_close,    ')', PAREN_CLOSE);
+  DEFINE_RESERVED(g_accolade_open,  '{', ACCOLADE_OPEN);
+  DEFINE_RESERVED(g_accolade_close, '}', ACCOLADE_CLOSE);
+  DEFINE_RESERVED(g_brace_open,     '[', BRACE_OPEN);
+  DEFINE_RESERVED(g_brace_close,    ']', BRACE_CLOSE);
 
   // String literal symbols
   // TODO: Create an extra tokentype called NONE, or EMPTY
-  DEFINE_RESERVED(g_double_quote, '"');
+  DEFINE_RESERVED(g_double_quote, '"', UNKNOWN);
 
   // Assignment
-  DEFINE_RESERVED(g_assignment, '=',  TokenType::ASSIGNMENT);
+  DEFINE_RESERVED(g_assignment, '=', ASSIGNMENT);
 
   // Arithmetic operators:
-  DEFINE_RESERVED(g_exponent,   '^', TokenType::EXPONENT);
-  DEFINE_RESERVED(g_plus,       '+', TokenType::PLUS);
-  DEFINE_RESERVED(g_minus,      '-', TokenType::MINUS);
-  DEFINE_RESERVED(g_multiplier, '*', TokenType::MULTIPLIER);
-  DEFINE_RESERVED(g_divider,    '/', TokenType::DIVIDER);
-  DEFINE_RESERVED(g_modulus,    '%', TokenType::MODULUS);
+  DEFINE_RESERVED(g_exponent,   '^', EXPONENT);
+  DEFINE_RESERVED(g_plus,       '+', PLUS);
+  DEFINE_RESERVED(g_minus,      '-', MINUS);
+  DEFINE_RESERVED(g_multiplier, '*', MULTIPLIER);
+  DEFINE_RESERVED(g_divider,    '/', DIVIDER);
+  DEFINE_RESERVED(g_modulus,    '%', MODULUS);
 
   // Assignment variants of Arithmetic operators:
-  DEFINE_RESERVED(g_increment, "++", TokenType::INCREMENT);
-  DEFINE_RESERVED(g_decrement, "--", TokenType::DECREMENT);
+  DEFINE_RESERVED(g_increment, "++", INCREMENT);
+  DEFINE_RESERVED(g_decrement, "--", DECREMENT);
 
   // TODO: Rename or structure these better in the future?
-  DEFINE_RESERVED(g_exponent_assignment,       "^=", TokenType::EXPONENT_ASSIGNMENT);
-  DEFINE_RESERVED(g_plus_assignment,           "+=", TokenType::PLUS_ASSIGNMENT);
-  DEFINE_RESERVED(g_minus_assignment,          "-=", TokenType::MINUS_ASSIGNMENT);
-  DEFINE_RESERVED(g_multiplication_assignment, "*=", TokenType::MULTIPLICATION_ASSIGNMENT);
-  DEFINE_RESERVED(g_division_assignment,       "/=", TokenType::DIVISION_ASSIGNMENT);
-  DEFINE_RESERVED(g_modulo_assignment,         "%=", TokenType::MODULO_ASSIGNMENT);
+  DEFINE_RESERVED(g_exponent_assignment,       "^=", EXPONENT_ASSIGNMENT);
+  DEFINE_RESERVED(g_plus_assignment,           "+=", PLUS_ASSIGNMENT);
+  DEFINE_RESERVED(g_minus_assignment,          "-=", MINUS_ASSIGNMENT);
+  DEFINE_RESERVED(g_multiplication_assignment, "*=", MULTIPLICATION_ASSIGNMENT);
+  DEFINE_RESERVED(g_division_assignment,       "/=", DIVISION_ASSIGNMENT);
+  DEFINE_RESERVED(g_modulo_assignment,         "%=", MODULO_ASSIGNMENT);
 
   // Regex operators:
-  DEFINE_RESERVED(g_ere_match, '~', TokenType::ERE_MATCH);
-  DEFINE_RESERVED(g_not_ere_match, "!~", TokenType::NOT_ERE_MATCH);
+  DEFINE_RESERVED(g_ere_match,     '~',  ERE_MATCH);
+  DEFINE_RESERVED(g_not_ere_match, "!~", NOT_ERE_MATCH);
 
   // Logic operators:
-  DEFINE_RESERVED(g_not, '!');
-  DEFINE_RESERVED(g_less_than, '<');
-  DEFINE_RESERVED(g_less_than_equal, '<');
+  DEFINE_RESERVED(g_not,             '!',  NOT);
+  DEFINE_RESERVED(g_less_than,       '<',  LESS_THAN);
+  DEFINE_RESERVED(g_less_than_equal, "<=", LESS_THAN_EQUAL);
 
-  DEFINE_RESERVED(g_greater_than, '>');
-  DEFINE_RESERVED(g_greater_than_equal, '>');
+  DEFINE_RESERVED(g_greater_than,       '>',  GREATER_THAN);
+  DEFINE_RESERVED(g_greater_than_equal, ">=", GREATER_THAN_EQUAL);
 
-  // Control flow symbols
-  DEFINE_RESERVED(g_comma, ',');
-  DEFINE_RESERVED(g_questionmark, '?');
-  DEFINE_RESERVED(g_colon, ':');
-  DEFINE_RESERVED(g_semicolon, ';');
+  // Control flow symbols:
+  DEFINE_RESERVED(g_comma,        ',', COMMA);
+  DEFINE_RESERVED(g_questionmark, '?', QUESTIONMARK);
+  DEFINE_RESERVED(g_colon,        ':', COLON);
+  DEFINE_RESERVED(g_semicolon,    ';', SEMICOLON);
 
   // Miscellaneous operators:
-  DEFINE_RESRVED(g_dollar_sign, '$');
-  DEFINE_RESRVED(g_end_of_line, '\n');
+  DEFINE_RESERVED(g_dollar_sign, '$',  DOLLAR_SIGN);
+  DEFINE_RESERVED(g_end_of_line, '\n', END_OF_LINE);
 
 // clang-format on
 }; // namespace symbols
