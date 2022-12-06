4630cc6de863d05a0cffc832e9776c9735023cb6
sappigeninja <sebasoerlemans@gmail.com>
2022-12-06 01:50:46 +0100

    Working on the Expression classes

--- src/expression.cpp
+++ src/expression.cpp
@@ -1,1 +1,36 @@
+#include <type_traits>
+#include <utility>
+
 #include "expression.hpp"
+
+
+Expression::Expression(TokenType t_tokentype)
+  :m_tokentype{t_tokentype}
+{}
+
+template<ExpressionChild T>
+Expression::Expression(TokenType t_tokentype, T&& t_expr)
+  :m_tokentype{t_tokentype}, m_left{std::make_unique<T>(t_expr)}
+{}
+
+Expression::Expression(Token t_token)
+  :m_tokentype{t_token.type()}
+{}
+
+auto Expression::tokentype() -> TokenType
+{
+  return m_tokentype;
+}
+
+auto Expression::left() -> Expression&
+{
+  return *m_left;
+}
+
+// auto Expression::left(Expression& t_expr) -> void
+// {
+//   m_left.
+// }
+
+Expression::~Expression()
+{}
--- src/expression.hpp
+++ src/expression.hpp
@@ -1,13 +1,45 @@
 #ifndef EXPRESSION_H
 #define EXPRESSION_H
 
+#include <memory>
+#include <list>
+#include <type_traits>
+
+#include "token.hpp"
+#include "tokentype.hpp"
+
+
+// Forward declarations:
+class Expression;
+
+// Concepts:
+template<typename T>
+concept ExpressionChild = std::is_base_of<Expression, T>::value;
+
+// Aliases:
+using ExpressionPointer = std::unique_ptr<Expression>;
+using ExpressionList = std::list<std::unique_ptr<Expression>>;
+
 class Expression {
-private:
+protected:
+  TokenType m_tokentype;
+  ExpressionPointer m_left;
 
 public:
-  Expression();
+  explicit Expression(TokenType t_tokentype);
 
-  
+  template<ExpressionChild T>
+  explicit Expression(TokenType t_tokentype, T&& t_expr);
+
+  explicit Expression(Token t_token);
+
+  auto tokentype() -> TokenType;
+  virtual auto left() -> Expression&;
+  // virtual auto left(Expression& t_expr) -> void;
+
+  virtual auto right() -> Expression& = 0;
+  virtual auto list() -> ExpressionList = 0;
+
   virtual ~Expression();
 };
 
