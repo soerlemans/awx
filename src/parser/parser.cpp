9e16f38ecb980d6521928d91d8ebb1ab021e46f6
sappigeninja <sebasoerlemans@gmail.com>
2022-12-20 12:42:46 +0100

    Actually working on the parser

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -4,93 +4,65 @@
 
 
 Parser::Parser(TokenStream t_token_stream)
-  : m_token_stream{t_token_stream}, m_toplevel{false}
+  : m_token_stream{t_token_stream}
 {}
 
-auto Parser::identifier(const Token& t_token) -> bool
-{}
-
-// Handle keyword related tokens
-// Return a bool if it is a keyword
-auto Parser::keyword(const Token& t_token) -> bool
+auto Parser::function() -> NodePointer
 {
-  using namespace reserved::keywords;
-
-  bool is_keyword{true};
-  switch(t_token.type())
-    {
-      case g_function.tokentype():
-        break;
-
-      case g_return.tokentype():
-        break;
-
-      case g_if.tokentype():
-        break;
-
-      case g_do.tokentype():
-        break;
-
-      case g_while.tokentype():
-        break;
-
-      case g_for.tokentype():
-        break;
-
-      default:
-        is_keyword = false;
-        break;
-    }
-
-  return is_keyword;
+  // TODO: Define Function node for this
 }
 
-auto Parser::string(const Token& t_token) -> bool
+// Check for valid toplevel form tokens
+auto Parser::toplevel() -> NodePointer
 {
-  bool is_string{false};
-
-  return is_string;
-}
-
-auto Parser::symbol(const Token& t_token) -> bool
-{
-  bool is_symbol{false};
-
-  return is_symbol;
-}
-
-// Parse toplevel forms
-auto Parser::toplevel() -> bool
-{
-  bool is_toplevel{true};
+  NodePointer node;
   const TokenType token_type{m_token_stream.token().type()};
 
+  // TODO: Create seperate functions for handling/defining the next tokens
   switch(token_type)
     {
       case TokenType::FUNCTION:
+        function();
+        break;
+
+      case TokenType::REGEX:
+        // Handle regex pattern
         break;
 
       case TokenType::IDENTIFIER:
+      case TokenType::INTEGER:
+      case TokenType::HEX:
+      case TokenType::FLOAT:
+        // Handle conditional
+        break;
+
+		// Field reference does not have its own unique token
+      case TokenType::DOLLAR_SIGN:
+        // handle field reference
         break;
 
       default:
-        is_toplevel = false;
+		// Error handling
         break;
     }
 
-  return is_toplevel;
+  return node;
 }
 
 auto Parser::parse() -> Ast
 {
   using namespace reserved;
 
-  // Ast ast;
+  Ast ast;
   for(; m_token_stream.eos(); m_token_stream.next())
-    if(toplevel())
+	{
+	  // Ast is pieced together from calling nested functions
+	  // That each return a Node
+	  ast.add(toplevel());
+	}
 
   // return ast;
-	  return {};
+  return {};
 }
 
 Parser::~Parser()
--- src/parser/parser.hpp
+++ src/parser/parser.hpp
@@ -7,28 +7,22 @@
 
 
 class Parser {
-private:
+  private:
   TokenStream m_token_stream;
 
-  // TODO: Checkout of this variable is needed
-  bool m_toplevel;
-
-public:
+  public:
   Parser(TokenStream t_token_stream);
 
   // Helper
-  auto identifier(const Token& t_token) -> bool;
-  auto keyword(const Token& t_token) -> bool;
-  auto numeric(const Token& t_token) -> bool;
-  auto string(const Token& t_token) -> bool;
-  auto symbol(const Token& t_token) -> bool;
+  auto function() -> NodePointer;
 
-  auto toplevel() -> bool;
+  auto toplevel() -> NodePointer;
 
-  auto parse() -> Ast;
+  // The parse() method should be virtual cause in the future we may want to
+  // have different dialects of AWX
+  virtual auto parse() -> Ast;
 
   ~Parser();
 };
 
 #endif /* PARSER_H */
-
