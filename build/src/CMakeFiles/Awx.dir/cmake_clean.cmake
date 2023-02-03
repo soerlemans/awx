5885cd817897ee898c569a710a791ff607daedfa
sappigeninja <sebasoerlemans@gmail.com>
2023-02-03 21:28:56 +0100

    Setting up CMake

---
+++ build/src/CMakeFiles/Awx.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Awx"
+  "Awx.pdb"
+  "CMakeFiles/Awx.dir/main.cpp.o"
+  "CMakeFiles/Awx.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Awx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/Awx.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Awx.
+# This may be replaced when dependencies are built.
